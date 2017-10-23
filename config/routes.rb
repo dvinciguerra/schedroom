54d9a495e6f7cd5c77ae278fb06acd106a6e7806
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-10-23 11:07:40 -0200

    Add some improvements and refactors to route config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
-
-	# backend (application api)
+  # backend (application api)
   namespace :api do
     namespace :v1 do
-      post '/user-token', as: 'user_token', to: 'user_token#create'
+      post :user_token,
+           path: '/user-token', to: 'user_token#create'
+
       get '/user-profile', to: 'user_profile#index'
 
-      resource '/room-schedule',
-               as: 'room_schedule',
-               controller: 'room_schedule',
-               only: %i[index show create update destroy]
+      resources :room_schedule,
+               path: '/room-schedules', only: %i[create index show destroy]
     end
   end
 
-	# frontend (web interface)
+  # frontend (web interface)
   root to: 'home#index'
   get 'auth/signin'
-
 end
